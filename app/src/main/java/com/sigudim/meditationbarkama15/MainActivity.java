ad307d776c54dc2869841ef591b5d98ad97d7df0
Sigalit <sigudim@gmail.com>
2016-03-30 15:20:56 +0300

    before icon change

--- app/src/main/java/com/sigudim/meditationbarkama15/MainActivity.java
+++ app/src/main/java/com/sigudim/meditationbarkama15/MainActivity.java
@@ -2,6 +2,13 @@ package com.sigudim.meditationbarkama15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import static com.sigudim.meditationbarkama15.R.raw.guided_meditation15;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ImageButton one = (ImageButton) this.findViewById(R.id.button1);
+        //MediaPlayer = mp;
+        final MediaPlayer mp = MediaPlayer.create(this, guided_meditation15);
+        //one.OnCreateContextMenuListener(new View.OnClickListener(){
+        one.setOnClickListener(new View.OnClickListener() {
+            // one.OnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
     }
 }
